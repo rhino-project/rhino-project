1df77e8f620320c48fd6548a3386de07a99a50d4
JP Rosevear <jprosevear@gmail.com>
2020-07-19 21:25:34 -0400

    [NUB-239] Associated full text search

--- gems/rhino_core/lib/rhino/resource/active_record/search.rb
+++ gems/rhino_core/lib/rhino/resource/active_record/search.rb
@@ -7,12 +7,12 @@ module Rhino
         extend ActiveSupport::Concern
 
         class_methods do
-          def rhino_search(fields)
+          def rhino_search(fields, associated_fields = {})
             include PgSearch::Model
 
             # FIXME: We can't autodiscover text fields because it causes a very
             # slow reload in the development server with bigger apps
-            pg_search_scope :search_text_fields, against: fields, using: { tsearch: { prefix: true } }
+            pg_search_scope :search_text_fields, against: fields, associated_against: associated_fields, using: { tsearch: { prefix: true } }
           end
         end
       end
