31b96d34be1efc49ce7c0a614b0d83a785ba9b82
JP Rosevear <jprosevear@gmail.com>
2020-09-05 09:09:01 -0400

    [NUB-280] Include Rhino::Resource::ActiveRecord automatically based on setting

--- gems/rhino_core/lib/rhino/engine.rb
+++ gems/rhino_core/lib/rhino/engine.rb
@@ -5,6 +5,10 @@ module Rhino
     config.autoload_paths << File.expand_path('../../lib', __dir__)
     config.autoload_paths << File.expand_path('../../app/resources', __dir__)
 
+    ActiveSupport.on_load(:active_record) do
+      include Rhino::Resource::ActiveRecord if Rhino.auto_include_active_record
+    end
+
     ActiveSupport.on_load(:active_storage_attachment) do
       include Rhino::Resource::ActiveStorage
     end
--- gems/rhino_core/test/dummy/app/models/resource_parent.rb
+++ gems/rhino_core/test/dummy/app/models/resource_parent.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ResourceParent < ApplicationRecord
-  include Rhino::Resource::ActiveRecord
-
   has_many :resources
 
   rhino_owner_base
