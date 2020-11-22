16a1c710e61985aab64172c5c78efac94dc614e9
JP Rosevear <jprosevear@gmail.com>
2020-11-22 09:07:18 -0500

    [NUB-292] Support tree/hierarchal style API endpoints

--- gems/rhino/lib/rhino/resource/serialization.rb
+++ gems/rhino/lib/rhino/resource/serialization.rb
@@ -6,9 +6,10 @@ module Rhino
       extend ActiveSupport::Concern
 
       included do
-        def to_caching_json
-          raise NotImplementedError, '#to_caching_json is not implemented'
-        end
+      end
+
+      def to_caching_json
+        raise NotImplementedError, '#to_caching_json is not implemented'
       end
     end
   end
