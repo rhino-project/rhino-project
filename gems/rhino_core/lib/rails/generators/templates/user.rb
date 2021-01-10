a23275415486252dc2f8bc2f476e8e1097ac7906
JP Rosevear <jprosevear@gmail.com>
2021-01-10 11:07:58 -0500

    [NUB-315] Remove organization relationships from user template

--- gems/rhino_core/lib/rails/generators/templates/user.rb
+++ gems/rhino_core/lib/rails/generators/templates/user.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 class User < Rhino::User
-  has_many :users_roles, dependent: :destroy
-  has_many :organizations, through: :users_roles
-  has_many :roles, through: :users_roles
 end
