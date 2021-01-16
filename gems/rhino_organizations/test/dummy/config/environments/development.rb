484d89c43ad8692a6ea05856045b32db60b1ef7b
JP Rosevear <jprosevear@gmail.com>
2021-01-16 12:14:35 -0500

    [NUB-363] Fix host URL in development

--- gems/rhino_organizations/test/dummy/config/environments/development.rb
+++ gems/rhino_organizations/test/dummy/config/environments/development.rb
@@ -61,4 +61,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  Rails.application.routes.default_url_options[:host] = ENV['ROOT_URL']
 end
