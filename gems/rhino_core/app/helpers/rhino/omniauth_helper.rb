110caeb4593f881a919aa6d355217f354add2587
JP Rosevear <jprosevear@gmail.com>
2021-01-20 16:07:36 -0500

    [NUB-369] [NUB-272] [NUB-7] [NUB-5] OmniAuth support with google, github, facebook

---
+++ gems/rhino_core/app/helpers/rhino/omniauth_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Rhino
+  module OmniauthHelper
+    def self.strategies
+      strategies = ENV.keys.map do |env|
+        match = /AUTH_(.*)_CLIENT_ID/.match(env)
+        next unless match
+
+        match[1].downcase.to_sym
+      end.compact.uniq
+
+      strategies += [:developer] if Rails.env.development?
+
+      strategies
+    end
+
+    def self.app_info(strategy)
+      return [] if strategy == :developer
+
+      ups = strategy.to_s.upcase
+      [ENV["AUTH_#{ups}_CLIENT_ID"], ENV["AUTH_#{ups}_SECRET_KEY"]] unless strategy == :developer
+    end
+  end
+end
