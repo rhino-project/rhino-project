1806814790b9854f959f61d6330e28c974289caa
JP Rosevear <jprosevear@gmail.com>
2021-01-31 08:32:02 -0500

    [NUB-388] Guard against multiple rhino_references calls

--- gems/rhino_core/test/lib/engine_test.rb
+++ gems/rhino_core/test/lib/engine_test.rb
@@ -21,4 +21,14 @@ class EngineTest < ActiveSupport::TestCase
     end
     assert_equal exp.message, 'EngineTest::BadResource has references [:user, :banner_attachment, :blog] that do not exist as associations'
   end
+
+  test 'raises error if rhino_references called multiple times' do
+    exp = assert_raises StandardError do
+      class BadResourceMultipleReferenceCalls < ApplicationRecord
+        rhino_references [:a]
+        rhino_references [:b]
+      end
+    end
+    assert_equal exp.message, 'rhino_references called multiple times for EngineTest::BadResourceMultipleReferenceCalls'
+  end
 end
