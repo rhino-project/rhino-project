8227d6cf52f92391479e9a2840cc7cca8fa0f244
JP Rosevear <jprosevear@gmail.com>
2021-02-20 07:04:38 -0500

    [NUB-304] Generator to create new modules

---
+++ gems/rhino_core/lib/generators/rhino/install/install_generator.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Rhino
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path('templates', __dir__)
+
+      def install_user
+        say 'Installing user to app/models'
+        template 'user.rb', 'app/models/user.rb'
+      end
+
+      def install_initializer
+        say 'Installing initializer'
+        template 'rhino.rb', 'config/initializers/rhino.rb'
+      end
+    end
+  end
+end
--- gems/rhino_core/lib/rails/generators/installer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-say 'Copying Rhino models to app/models'
-copy_file "#{__dir__}/templates/user.rb", 'app/models/user.rb'
-copy_file "#{__dir__}/templates/rhino.rb", 'config/initializers/rhino.rb'
