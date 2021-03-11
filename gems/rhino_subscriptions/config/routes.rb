6dec31a087ac1cf065f1e81fd7c2681c3a1365a9
sabatf <sabatayebi.far@gmail.com>
2021-03-11 18:15:37 -0500

    [NUB-423] modify subscription module to be compatible with organizations

--- gems/rhino_subscriptions/config/routes.rb
+++ gems/rhino_subscriptions/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     scope Rhino.namespace do
 
       get 'subscription/prices' , to: 'rhino/stripe#prices'
-      get 'subscription/customer' ,to: 'rhino/stripe#customer'
+      post 'subscription/customer' ,to: 'rhino/stripe#customer'
       get 'subscription/subscriptions' ,to: 'rhino/stripe#subscriptions'
       post 'subscription/create-checkout-session' ,to: 'rhino/stripe#create_checkout_session'
 
