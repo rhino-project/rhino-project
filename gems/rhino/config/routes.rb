3fd78204f756548cd60bec750c565d31d36764f1
JP Rosevear <jprosevear@gmail.com>
2021-04-15 04:20:27 -0400

    [NUB-480] Remove filestack and generic attachment model that are not used

--- gems/rhino/config/routes.rb
+++ gems/rhino/config/routes.rb
@@ -11,12 +11,6 @@ Rails.application.routes.draw do
     Rhino.resource_classes.each do |m|
       resources m.route_key, path: m.route_path, controller: m.controller_name, only: m.routes, rhino_resource: m.name
     end
-
-    post 'filestack_pictures/upload_policy', to: 'filestack_pictures#upload_policy'
-    post 'filestack_pictures', to: 'filestack_pictures#create'
-
-
-    # get 'info/models', controller: 'rhino/crud', action: 'models' if Rails.env.development?
   end
 
   match "*#{Rhino.namespace}", to: 'rhino/base#not_found', via: :all, constraints: lambda { |request|
