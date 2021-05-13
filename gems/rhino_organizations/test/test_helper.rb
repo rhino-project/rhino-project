619f4d0beb1e03bf4102bfa5f1ba92dd4900d8da
JP Rosevear <jprosevear@gmail.com>
2021-05-13 17:15:36 -0400

    [NUB-526] Coverage for all full modules

--- gems/rhino_organizations/test/test_helper.rb
+++ gems/rhino_organizations/test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+if ENV["COVERAGE"]
+  require "simplecov"
+
+  SimpleCov.start "rails"
+end
+
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
@@ -16,4 +22,25 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures :all
+end
+
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # make each process running tests a separate command for simple cov.
+  # in the end, they will all get merged and not overwrite each other's results
+  # https://github.com/simplecov-ruby/simplecov/issues/718#issuecomment-538201587
+  if ENV["COVERAGE"]
+    parallelize_setup do |worker|
+      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+    end
+
+    parallelize_teardown do |_worker|
+      SimpleCov.result
+    end
+  end
+
+  # Add more helper methods to be used by all tests here...
+  include FactoryBot::Syntax::Methods
 end
