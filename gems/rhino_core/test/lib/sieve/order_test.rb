2a146861eff46827d7ebdc73511a4d616a29aee8
Silas Pedrosa <silaspedrosa@gmail.com>
2021-05-20 11:47:34 -0300

    [NUB-546] Support multiple order clauses

--- gems/rhino_core/test/lib/sieve/order_test.rb
+++ gems/rhino_core/test/lib/sieve/order_test.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+require "test_helper"
+
 class RhinoSieveOrderTestHelper < ActionDispatch::IntegrationTest
   protected
+  def setup
+    seed
+    sign_in
+  end
+
   def sign_in
     post "/api/auth/sign_in", params: {
       email: @current_user.email,
@@ -27,6 +34,61 @@ class RhinoSieveOrderTestHelper < ActionDispatch::IntegrationTest
     assert_response :ok
     @json = JSON.parse(@response.body)
   end
+
+  def assert_order(results, *instances)
+    instances.each_with_index do |instance, idx|
+      assert_equal instance.id, results[idx]["id"]
+    end
+  end
+end
+
+class RhinoSieveOrderBasicTest < RhinoSieveOrderTestHelper
+  def url
+    "/api/blog_posts"
+  end
+
+  test "does not fail when order param is nil" do
+    @params = nil
+    fetch
+  end
+
+  test "does not fail when order param is empty string" do
+    @params = ""
+    fetch
+  end
+
+  test "does not fail when order param is a string containing only spaces" do
+    @params = "   "
+    fetch
+  end
+end
+
+class RhinoSieveOrderMultipleClausesTest < RhinoSieveOrderTestHelper
+  def url
+    "/api/blog_posts"
+  end
+
+  def seed # rubocop:disable Metrics/AbcSize
+    @current_user = create :user
+    @blog = create :blog, user: @current_user, published_at: Time.zone.now
+
+    now = Time.zone.now
+    @instance1 = create :blog_post, blog: @blog, created_at: Time.zone.now, updated_at: now
+    @instance2 = create :blog_post, blog: @blog, created_at: Time.zone.now - 1.day, updated_at: now
+    @instance3 = create :blog_post, blog: @blog, created_at: Time.zone.now + 1.day, updated_at: now - 1.day
+  end
+
+  test "ordering sieve works with two clauses" do
+    @params = "updated_at,-created_at"
+    fetch
+
+    assert_order @json["results"], @instance3, @instance1, @instance2
+
+    @params = "-updated_at,created_at"
+    fetch
+
+    assert_order @json["results"], @instance2, @instance1, @instance3
+  end
 end
 
 class RhinoSieveOrderTest < RhinoSieveOrderTestHelper
@@ -34,25 +96,16 @@ class RhinoSieveOrderTest < RhinoSieveOrderTestHelper
     "/api/blog_posts"
   end
 
-  def assert_order(results, *instances)
-    instances.each_with_index do |instance, idx|
-      assert_equal instance.id, results[idx]["id"]
-    end
-  end
-
   test "BlogPost.all sql orders by created_at DESC" do
     assert_match(/ORDER BY .*created_at.? DESC/, BlogPost.all.to_sql)
   end
 
   test "BlogPost default scope orders by created_at DESC" do
-    seed
     results = BlogPost.all
     assert_order results, @newest_instance, @middle_instance, @oldest_instance
   end
 
   test "order[created_at] overrides default_ordering to created_at ASC" do
-    seed
-    sign_in
     @params = "created_at"
     fetch
 
@@ -60,8 +113,6 @@ class RhinoSieveOrderTest < RhinoSieveOrderTestHelper
   end
 
   test "order[-created_at] overrides default ordering to created_at DESC" do
-    seed
-    sign_in
     @params = "-created_at"
     fetch
 
@@ -69,8 +120,6 @@ class RhinoSieveOrderTest < RhinoSieveOrderTestHelper
   end
 
   test "ordering by non-existing columns should ignore order param and use default ordering" do
-    seed
-    sign_in
     @params = "zzzcreated_atzzz"
     fetch
 
@@ -78,8 +127,6 @@ class RhinoSieveOrderTest < RhinoSieveOrderTestHelper
   end
 
   test "ordering works for aliased fields like created_at <> aliased_creation_date" do
-    seed
-    sign_in
     @params = "aliased_creation_date"
     fetch
 
@@ -87,8 +134,6 @@ class RhinoSieveOrderTest < RhinoSieveOrderTestHelper
   end
 
   test "ordering puts nulls last for asc" do
-    seed
-    sign_in
     @params = "published_at"
     fetch("/api/blogs")
 
@@ -96,8 +141,6 @@ class RhinoSieveOrderTest < RhinoSieveOrderTestHelper
   end
 
   test "ordering puts nulls last for desc" do
-    seed
-    sign_in
     @params = "-published_at"
     fetch("/api/blogs")
 
