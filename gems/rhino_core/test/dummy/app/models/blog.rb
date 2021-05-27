3f7e9e9b423bc672b183513db0a8344d1072760a
JP Rosevear <jprosevear@gmail.com>
2021-05-27 10:06:57 -0400

    [NUB-255] Fix build

--- gems/rhino_core/test/dummy/app/models/blog.rb
+++ gems/rhino_core/test/dummy/app/models/blog.rb
@@ -13,7 +13,7 @@ class Blog < ApplicationRecord
   has_one_attached :banner
 
   rhino_owner_base
-  rhino_references [:user, {blogs_categories: %i[blog category]}, :banner_attachment, :blog_posts]
+  rhino_references [:user, { blogs_categories: %i[blog category] }, :banner_attachment, :blog_posts]
   rhino_properties_format blogs_categories: :join_table_simple
   rhino_search [:title]
 
