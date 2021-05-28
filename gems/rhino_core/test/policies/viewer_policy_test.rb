8e6ace62c0faaba53026e0e053e8c58edf35262e
JP Rosevear <jprosevear@gmail.com>
2021-05-28 01:28:03 -0400

    [NUB-588] Add missing file

---
+++ gems/rhino_core/test/policies/viewer_policy_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Rhino::ViewerPolicyTest < Rhino::TestCase::Policy
+  def setup
+    @current_user = create :user
+  end
+
+  %i[index show create update destroy].each do |action_type|
+    test "#{testing_policy} does not allow #{action_type} for unauthenticated user" do
+      assert_not_permit nil, Dummy, action_type
+    end
+  end
+
+  # Current user
+  %i[index show].each do |action_type|
+    test "#{testing_policy} allows #{action_type} for authenticated user" do
+      assert_permit @current_user, Dummy, action_type
+    end
+  end
+
+  %i[create destroy update].each do |action_type|
+    test "#{testing_policy} does not allow #{action_type} for authenticated user" do
+      assert_not_permit @current_user, Dummy, action_type
+    end
+  end
+end
