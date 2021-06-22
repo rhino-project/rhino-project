7063a86ebc2fd79697d2daf0f9de1bab9d205c4d
Silas Pedrosa <silaspedrosa@gmail.com>
2021-06-22 09:57:30 -0300

    [NUB-683] ConfirmationsController not working

--- gems/rhino/app/overrides/devise_token_auth/confirmations_controller_override.rb
+++ gems/rhino/app/overrides/devise_token_auth/confirmations_controller_override.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
-class DeviseTokenAuth::ConfirmationsController
+module DeviseTokenAuth::ConfirmationsController::Extensions
   # devise_token_auth raises an error if the confirmation token can't be found
   def show
     super
   rescue StandardError
     redirect_to ENV["FRONT_END_URL"]
   end
+end
+
+class DeviseTokenAuth::ConfirmationsController
+  prepend DeviseTokenAuth::ConfirmationsController::Extensions
 end
