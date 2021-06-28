5504a04619bf90a4935ff3fb9d893c21b5f51270
Silas Pedrosa <silaspedrosa@gmail.com>
2021-06-28 15:09:34 -0300

    [NUB-637] Sloth: embed users_role in Organizations so client knows the user's role

--- gems/rhino_organizations/lib/generators/rhino_organizations/install/templates/organization.rb
+++ gems/rhino_organizations/lib/generators/rhino_organizations/install/templates/organization.rb
@@ -3,4 +3,6 @@
 class Organization < Rhino::Organization
   has_many :users_roles, dependent: :destroy
   has_many :users, through: :users_roles
+
+  rhino_references [{ users_roles: [:role] }]
 end
