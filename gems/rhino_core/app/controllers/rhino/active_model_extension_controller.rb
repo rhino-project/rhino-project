341a2929f7c00f88d2fc08cb544add600769b0e8
JP Rosevear <jprosevear@gmail.com>
2021-07-14 00:25:11 -0400

    [NUB-750] Use Rhino::Permit for crud and active_model_extension controllers

--- gems/rhino_core/app/controllers/rhino/active_model_extension_controller.rb
+++ gems/rhino_core/app/controllers/rhino/active_model_extension_controller.rb
@@ -2,10 +2,10 @@
 
 module Rhino
   class ActiveModelExtensionController < BaseController
-    thread_mattr_accessor :iot_client
+    include Rhino::Permit
 
     # Confirm we are calling authorize and scope correctly
-    # after_action :verify_authorized
+    after_action :verify_authorized
     # after_action :verify_policy_scoped, only: %i[index show]
 
     def create
@@ -34,7 +34,7 @@ module Rhino
     end
 
     def update
-      @model = klass.new(permit_and_transform(klass.new))
+      @model = authorize klass.new(permit_and_transform(klass.new))
       # FIXME: Return updated model
       @model.backing_store_update
 
@@ -47,46 +47,5 @@ module Rhino
 
       permit_and_render
     end
-
-    protected
-      # The params are wrapped with 'rest' if the rest_controller is called directly
-      # instead of via a customization class
-      #
-      # Protected to prevent polluting the action method
-      #
-      # FIXME: Couldn't find a way to call wrap_parameters appropriately
-      # FIXME: Will some sort of namespacing break this?
-      def pundit_params_for(_record)
-        params.require(controller_name)
-      end
-
-    private
-      def client
-        return iot_client if iot_client
-
-        Aws::IoT::Client.new
-      end
-
-      def find_resource(scope = klass.all)
-        scope = scope.friendly if scope.respond_to? :friendly
-        scope.find(params[:id])
-      end
-
-      def permit_and_transform(model = @model)
-        klass.transform_params(permitted_attributes(model))
-      end
-
-      def permit_model(model = @model)
-        model_policy = Pundit.policy(current_user, model)
-
-        model_params = ActionController::Parameters.new(model.to_caching_json)
-        model_params[:can_current_user_edit] = model_policy.update?
-        model_params[:can_current_user_destroy] = model_policy.destroy?
-        model_params.permit(model_policy.permitted_attributes_for_show + %i[can_current_user_edit can_current_user_destroy])
-      end
-
-      def permit_and_render
-        render json: permit_model
-      end
   end
 end
