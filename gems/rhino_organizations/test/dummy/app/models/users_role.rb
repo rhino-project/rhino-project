20a56c1cc4e59574dcea51c01efc4d6f752db81a
JP Rosevear <jprosevear@gmail.com>
2021-07-14 18:04:59 -0400

    [NUB-748] Move rhino config for UsersRole to abstract model

--- gems/rhino_organizations/test/dummy/app/models/users_role.rb
+++ gems/rhino_organizations/test/dummy/app/models/users_role.rb
@@ -4,8 +4,4 @@ class UsersRole < Rhino::UsersRole
   belongs_to :user
   belongs_to :organization
   belongs_to :role
-
-  rhino_policy :users_role
-  rhino_owner :organization
-  rhino_references %i[role organization]
 end
