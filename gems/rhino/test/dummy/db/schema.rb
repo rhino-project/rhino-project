66d5899cd3e6a892e3de4432213b3a22a73a3454
JP Rosevear <jprosevear@gmail.com>
2021-12-28 17:33:20 -0500

    [NUB-885] Consolidate invitations into org module

--- gems/rhino/test/dummy/db/schema.rb
+++ gems/rhino/test/dummy/db/schema.rb
@@ -161,16 +161,8 @@ ActiveRecord::Schema.define(version: 2021_11_14_204921) do
     t.datetime "updated_at", null: false
     t.boolean "allow_password_change", default: false, null: false
     t.boolean "approved", default: false
-    t.string "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer "invitation_limit"
-    t.integer "invited_by_id"
-    t.string "invited_by_type"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
