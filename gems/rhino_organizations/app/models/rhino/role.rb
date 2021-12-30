8388a22146fd7aa7c91506b99fe32aac5ce8bace
JP Rosevear <jprosevear@gmail.com>
2021-12-30 11:49:14 -0500

    [NUB-847] Validate role names

--- gems/rhino_organizations/app/models/rhino/role.rb
+++ gems/rhino_organizations/app/models/rhino/role.rb
@@ -4,6 +4,8 @@ module Rhino
   class Role < ApplicationRecord
     self.abstract_class = true
 
+    validates :name, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z]+\z/, message: "alpha characters only" }
+
     rhino_owner_global
   end
 end
