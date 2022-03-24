6b3cc50dfa650640d11b8691ee73c66c49a09eb7
JP Rosevear <jprosevear@gmail.com>
2022-03-24 10:11:46 -0400

    [NUB-980] Isolate class naming to prevent re-opening

--- gems/rhino/test/lib/validators/country_validator_test.rb
+++ gems/rhino/test/lib/validators/country_validator_test.rb
@@ -24,13 +24,13 @@ class CountryValidatorTest < ActiveSupport::TestCase
   end
 
   test "validates alpha2 country with allow_blank" do
-    class CountryAlpha2 < Country
+    class CountryAlpha2Blank < Country
       validates :country, country: { allow_blank: true }
     end
 
-    assert_predicate CountryAlpha2.new, :valid?
-    assert_predicate CountryAlpha2.new(country: "ZZ"), :invalid?
-    assert_predicate CountryAlpha2.new(country: "CA"), :valid?
+    assert_predicate CountryAlpha2Blank.new, :valid?
+    assert_predicate CountryAlpha2Blank.new(country: "ZZ"), :invalid?
+    assert_predicate CountryAlpha2Blank.new(country: "CA"), :valid?
   end
 
   test "validates alpha3 country" do
@@ -45,13 +45,13 @@ class CountryValidatorTest < ActiveSupport::TestCase
   end
 
   test "validates alpha3 country with allow_blank" do
-    class CountryAlpha3 < Country
+    class CountryAlpha3Blank < Country
       validates :country, country: { alpha3: true, allow_blank: true }
     end
 
-    assert_predicate CountryAlpha3.new, :valid?
-    assert_predicate CountryAlpha3.new(country: "ZZZ"), :invalid?
-    assert_predicate CountryAlpha3.new(country: "CAN"), :valid?
-    assert_predicate CountryAlpha3.new(country: "CA"), :invalid?
+    assert_predicate CountryAlpha3Blank.new, :valid?
+    assert_predicate CountryAlpha3Blank.new(country: "ZZZ"), :invalid?
+    assert_predicate CountryAlpha3Blank.new(country: "CAN"), :valid?
+    assert_predicate CountryAlpha3Blank.new(country: "CA"), :invalid?
   end
 end
