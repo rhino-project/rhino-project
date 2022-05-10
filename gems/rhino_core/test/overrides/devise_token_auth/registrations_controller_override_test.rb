13af2f74f7c97d934760f2c34b8f5e1a95fc9934
JP Rosevear <jprosevear@gmail.com>
2022-05-10 07:49:47 -0400

    [NUB-1026] Allow signup to be disabled

---
+++ gems/rhino_core/test/overrides/devise_token_auth/registrations_controller_override_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "minitest/autorun"
+
+class OverridesDeviseTokenAuthRegistrationsControllerOverrideTest < Rhino::TestCase::Override
+  test "devise_token_auth registrations controller is overridden" do
+    assert_overridden "#{__dir__}/../../../app/overrides/devise_token_auth/registrations_controller_override.rb",
+                      DeviseTokenAuth::RegistrationsController, :create
+  end
+end
+
+class DeviseTokenAuthRegistrationsControllerOverrideTest < Rhino::TestCase::ControllerTest
+  test "user can sign up when allowed" do
+    register_user
+
+    assert_response_ok
+  end
+
+  test "user cannot sign up when not allowed" do
+    Rhino.stub :allow_signup, false do
+      register_user
+    end
+
+    assert_response_unauthorized
+  end
+
+  private
+    def register_user
+      post user_registration_path,
+           params: { email: "test@example.com", password: "password", password_confirmation: "password" }, xhr: true, as: :json
+    end
+end
