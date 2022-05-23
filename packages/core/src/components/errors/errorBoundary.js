eb55f2a31ce7cc20a2af1599da1e426092f28c99
Fabio Barros <fabio@nubinary.com>
2022-05-23 15:26:05 -0300

    [NUB-969] Error boundary fails to display message (white screen of death)

--- packages/core/src/components/errors/errorBoundary.js
+++ packages/core/src/components/errors/errorBoundary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Alert } from 'reactstrap';
+import Rollbar from 'rollbar';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class ErrorBoundary extends React.Component {
       error: error,
       errorInfo: errorInfo
     });
-    throw error;
+    Rollbar.error(error);
   }
 
   render() {
