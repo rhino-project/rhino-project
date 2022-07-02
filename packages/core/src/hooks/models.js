a59d73797ddd76db17cd175f999ea0f428f4014f
JP Rosevear <jprosevear@gmail.com>
2022-07-02 11:45:52 -0400

    [NUB-151] Frontend Segment integration

--- packages/core/src/hooks/models.js
+++ packages/core/src/hooks/models.js
@@ -1,6 +1,6 @@
 import { isObject } from 'lodash';
 import { useMemo } from 'react';
-import { getModel } from 'rhino/utils/models';
+import { getModel, hasModule } from 'rhino/utils/models';
 
 /**
  * @typedef {import('../utils/models.js').Model} Model
@@ -18,3 +18,48 @@ import { getModel } from 'rhino/utils/models';
  */
 export const useModel = (model) =>
   useMemo(() => (isObject(model) ? model : getModel(model)), [model]);
+
+/**
+ * Return whether a module is enabled
+ *
+ * @param {string} module - The module nmae
+ * @returns {Boolean} Whether the module exists
+ *
+ * @example
+ *    const enabled = useHasModule('rhino_organizations')
+ */
+export const useHasModule = (module) =>
+  useMemo(() => hasModule(module), [module]);
+
+/**
+ * Whether organizations module is enabled
+ *
+ * @returns {Boolean} If organizations module exists
+ *
+ * @example
+ *    const enabled = useHasOrganizationsModule()
+ */
+export const useHasOrganizationsModule = () =>
+  useHasModule('rhino_organizations');
+
+/**
+ * Whether notifications module is enabled
+ *
+ * @returns {Boolean} If notifications module exists
+ *
+ * @example
+ *    const enabled = useHasNotificationsModule()
+ */
+export const useHasNotificationsModule = () =>
+  useHasModule('rhino_notifications');
+
+/**
+ * Whether subscriptions module is enabled
+ *
+ * @returns {Boolean} If subscriptions module exists
+ *
+ * @example
+ *    const enabled = useHasSubscriptionsModule()
+ */
+export const useHasSubscriptionsModule = () =>
+  useHasModule('rhino_subscriptions');
