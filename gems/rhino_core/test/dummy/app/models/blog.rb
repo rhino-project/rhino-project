9e0eb4d180610c0b67b920935a5e65e56f0c5b91
JP Rosevear <jprosevear@gmail.com>
2022-07-06 14:39:31 -0400

    [NUB-998] Readable names override support

--- gems/rhino_core/test/dummy/app/models/blog.rb
+++ gems/rhino_core/test/dummy/app/models/blog.rb
@@ -14,6 +14,7 @@ class Blog < ApplicationRecord
 
   rhino_owner_base
   rhino_references [:user, { blogs_categories: %i[blog category] }, :banner_attachment, :blog_posts]
+  rhino_properties_readable_name title: "Blog Title"
   rhino_properties_format blogs_categories: :join_table_simple, banner_attachment: :image, country: :country
   rhino_search [:title]
 
