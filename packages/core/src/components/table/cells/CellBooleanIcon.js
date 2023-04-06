fcf3a19baaf8534e156db9167ce8c661493bc786
Bruno Cipolla <brunocmoreira@live.com>
2023-04-06 10:54:28 -0300

    [NUB-1275] ModelCellBooleanIcon
    [NUB-1275] ModelCellBooleanIcon
    
    [NUB-1275] ModelCellBooleanIcon
    
    remove: iconClass

---
+++ packages/core/src/components/table/cells/CellBooleanIcon.js
@@ -0,0 +1,20 @@
+import { useMemo } from 'react';
+import { Icon } from 'rhino/components/icons';
+import { useTableInheritedProps } from '../../../hooks/table';
+
+const CellBooleanIcon = ({
+  trueIcon = 'check',
+  falseIcon = 'x',
+  empty = 'dash',
+  ...props
+}) => {
+  const { getValue, inheritedProps } = useTableInheritedProps(props);
+  const value = useMemo(() => {
+    if (getValue() == null) return empty;
+    return getValue() ? trueIcon : falseIcon;
+  }, [empty, getValue, trueIcon, falseIcon]);
+
+  return <Icon icon={value} {...inheritedProps} />;
+};
+
+export default CellBooleanIcon;
