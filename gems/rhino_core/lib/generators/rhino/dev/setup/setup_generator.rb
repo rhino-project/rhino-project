9825c94c30af6618d41c0a1830167c5cb233ce38
JP Rosevear <jp@nubinary.com>
2023-07-23 16:03:02 -0400

    [NUB-1418] Allow rhino:dev:setup to be configured only with command line options and no prompt

--- gems/rhino_core/lib/generators/rhino/dev/setup/setup_generator.rb
+++ gems/rhino_core/lib/generators/rhino/dev/setup/setup_generator.rb
@@ -4,7 +4,20 @@ module Rhino
   module Generators
     module Dev
       class SetupGenerator < ::Rails::Generators::Base
-        attr_reader :server_port, :client_port, :client_source, :db_name, :db_user, :db_password
+        class_option :prompt, type: :boolean, default: true
+        class_option :server_port, type: :numeric
+        class_option :client_port, type: :numeric
+        class_option :client_source, type: :string
+        class_option :db_name, type: :string
+        class_option :db_user, type: :string
+        class_option :db_password, type: :string
+
+        attr_reader :server_port,
+                    :client_port,
+                    :client_source,
+                    :db_name,
+                    :db_user,
+                    :db_password
 
         source_root File.expand_path("templates", __dir__)
 
@@ -20,13 +33,17 @@ module Rhino
         def install_hooks
           say "Installing git hooks"
           # Hooks need to be executable
-          copy_file "prepare-commit-msg", ".git/hooks/prepare-commit-msg", mode: :preserve
-          copy_file "prepare-commit-msg", client_file(".git/hooks/prepare-commit-msg"), mode: :preserve
+          copy_file "prepare-commit-msg",
+                    ".git/hooks/prepare-commit-msg",
+                    mode: :preserve
+          copy_file "prepare-commit-msg",
+                    client_file(".git/hooks/prepare-commit-msg"),
+                    mode: :preserve
         end
 
         private
           def server_extension
-            Rails.root.basename.to_s.match(/[-_]server/)[0]
+            Rails.root.basename.to_s.match(/[-_]?server/)[0]
           end
 
           def client_extension
@@ -34,7 +51,7 @@ module Rhino
           end
 
           def server_port_default
-            ENV["PORT"] || 3000
+            options[:server_port] || ENV["PORT"] || 3000
           end
 
           def client_port_default
@@ -42,34 +59,47 @@ module Rhino
           end
 
           def client_source_default
-            ENV["CLIENT_SOURCE"] || Rails.root.sub(server_extension, client_extension)
+            ENV["CLIENT_SOURCE"] ||
+              Rails.root.sub(server_extension, client_extension)
           end
 
           def db_name_default
-            ENV["DB_NAME"] || File.basename(Rails.root).sub(server_extension, "")
+            db_name =
+              options[:db_name] || ENV["DB_NAME"] ||
+              File.basename(Rails.root).sub(server_extension, "")
+
+            return db_name if db_name.present?
+
+            File.basename(Rails.root.parent)
           end
 
           def db_user_default
-            ENV["DB_USERNAME"]
+            options[:db_user] || ENV["DB_USERNAME"]
           end
 
           def db_password_default
-            ENV["DB_PASSWORD"]
+            options[:db_password] || ENV["DB_PASSWORD"]
           end
 
           def collect_env_info
-            @server_port = ask "Server Port?", default: server_port_default
-            @client_port = ask "Client Port?", default: client_port_default
+            @server_port = ask_prompt("Server Port?", server_port_default)
+            @client_port = ask_prompt("Client Port?", client_port_default)
 
-            @client_source = ask "Client Source?", default: client_source_default
+            @client_source = ask_prompt("Client Source?", client_source_default)
 
-            @db_name = ask "Database?", default: db_name_default
-            @db_user = ask "Database User?", default: db_user_default
-            @db_password = ask "Database Password?", default: db_password_default
+            @db_name = ask_prompt("Database?", db_name_default)
+            @db_user = ask_prompt("Database User?", db_user_default)
+            @db_password = ask_prompt("Database Password?", db_password_default)
           end
 
           def client_file(file)
             "#{@client_source}/#{file}"
+          end
+
+          def ask_prompt(message, default)
+            return default unless options[:prompt]
+
+            ask(message, default:)
           end
       end
     end
