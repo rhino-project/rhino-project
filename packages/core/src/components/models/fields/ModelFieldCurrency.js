a89209c59e72024cf10a328fe9ef282bad689beb
Mateus Cruz <mateus@nubinary.com>
2023-09-01 13:36:34 -0400

    [NUB-1444] Coerce empty decimals to null before validating
    - Fix rendering of form feedback for currency fields

--- packages/core/src/components/models/fields/ModelFieldCurrency.js
+++ packages/core/src/components/models/fields/ModelFieldCurrency.js
@@ -1,10 +1,15 @@
-import PropTypes from 'prop-types';
-import { FieldCurrency } from '../../forms/fields/FieldCurrency';
-import { InputGroup } from 'reactstrap';
 import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { InputGroup } from 'reactstrap';
+import { useFieldError } from 'rhino/hooks/form';
 import { useGlobalComponent } from 'rhino/hooks/overrides';
+import { FieldCurrency } from '../../forms/fields/FieldCurrency';
 
-const ModelFieldCurrencyBase = ({ overrides, error, ...props }) => {
+const ModelFieldCurrencyBase = ({ overrides, ...props }) => {
+  // This bug was added in d08e56c. The error is not really passed down to the component
+  // as a prop, it comes from the react form hooks. So error was always undefined.
+  // FieldCurrency is able to get the error from the hooks and display the feedback.
+  const error = useFieldError(props.path);
   return (
     <InputGroup
       className={classnames({
@@ -12,7 +17,7 @@ const ModelFieldCurrencyBase = ({ overrides, error, ...props }) => {
       })}
     >
       <span className="input-group-text">$</span>
-      <FieldCurrency error={error} {...props} />
+      <FieldCurrency {...props} />
     </InputGroup>
   );
 };
