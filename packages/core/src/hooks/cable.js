63e9a22984284ab75fac8593f22cf2b3bd4c1f18
JP Rosevear <jp@nubinary.com>
2024-01-01 16:02:55 -0500

    [NUB-1567] Fully switch over to VITE_ env vars and stop export from config

--- packages/core/src/hooks/cable.js
+++ packages/core/src/hooks/cable.js
@@ -1,8 +1,8 @@
 import { createConsumer } from '@rails/actioncable';
 import { useMemo } from 'react';
-import { API_ROOT_PATH } from 'rhino/config';
+import env from 'rhino/config/env';
 
-const CABLE_PATH = `${API_ROOT_PATH}/cable`;
+const CABLE_PATH = `${env.API_ROOT_PATH}/cable`;
 
 export const useCable = () => {
   const consumer = useMemo(() => createConsumer(CABLE_PATH), []);
