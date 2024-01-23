e8682ddb9ea9f92d8a7378c14ab81a84c9c8d890
JP Rosevear <jp@nubinary.com>
2024-01-23 09:05:43 -0500

    feat(vite-plugin-rhino): resolveID in order to replace rhino.config with local project copy

--- packages/vite-plugin-rhino/src/index.ts
+++ packages/vite-plugin-rhino/src/index.ts
@@ -1,13 +1,22 @@
 import { Plugin } from 'vite'
+import path from 'path'
 
 export function RhinoProjectVite(): Plugin {
   return {
     name: 'vite-plugin-rhino',
-    config: async () => ({
+    config: () => ({
       optimizeDeps: {
         // Don't process with esbuild so that we can use resolveId for rhino.config
         exclude: ['@rhino-project/config'],
       },
     }),
+    resolveId(source) {
+      if (source === 'rhino.config') {
+        // Replace 'rhino.config' with the path to the local file
+        // FIXME: Allow the location to be configured
+        return path.resolve(__dirname, 'src/rhino.config.js')
+      }
+      return null // Other imports are handled as usual
+    },
   }
 }
