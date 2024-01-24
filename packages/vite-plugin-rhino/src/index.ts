b99217f08119db6c92f9014f14a4324a595e076e
JP Rosevear <jp@nubinary.com>
2024-01-23 23:22:45 -0500

    fix(vite-plugin-rhino): Absolute rhino.config.js path (#21)

--- packages/vite-plugin-rhino/src/index.ts
+++ packages/vite-plugin-rhino/src/index.ts
@@ -1,7 +1,9 @@
 import { Plugin } from 'vite'
-import path from 'path'
+import { join } from 'path'
 
 export function RhinoProjectVite(): Plugin {
+  let ROOT: string
+
   return {
     name: 'vite-plugin-rhino',
     config: () => ({
@@ -10,12 +12,18 @@ export function RhinoProjectVite(): Plugin {
         exclude: ['@rhino-project/config'],
       },
     }),
+
+    configResolved(vite) {
+      ROOT = vite.root
+    },
+
     resolveId(source) {
       if (source === 'rhino.config') {
         // Replace 'rhino.config' with the path to the local file
         // FIXME: Allow the location to be configured
-        return { id: '/src/rhino.config.js', external: 'relative' }
+        return join(ROOT, 'src/rhino.config.js')
       }
+
       return null // Other imports are handled as usual
     },
   }
