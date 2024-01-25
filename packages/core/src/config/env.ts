cfa7fedb1c63f08e440aecac78f79399be87ad27
JP Rosevear <jp@nubinary.com>
2024-01-25 12:35:58 -0500

    [NUB-1617] Fix VITE_DESIGN_SYSTEM_ENABLED var name

--- packages/core/src/config/env.ts
+++ packages/core/src/config/env.ts
@@ -2,7 +2,7 @@ export default {
   API_ROOT_PATH: import.meta.env.VITE_API_ROOT_PATH,
   DESIGN_SYSTEM_ENABLED:
     import.meta.env.DEV ||
-    import.meta.env.VITE_DESIGN_SYSTEM_ENABLED_DESIGN_SYSTEM_ENABLED === 'true',
+    import.meta.env.VITE_DESIGN_SYSTEM_ENABLED === 'true',
   ROLLBAR_ACCESS_TOKEN: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
   ROLLBAR_ENV: import.meta.env.VITE_ROLLBAR_ENV || import.meta.env.MODE,
   ROLLBAR_ENABLED:
