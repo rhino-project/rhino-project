002e27e8f3e4af767b49126b55c209497e886828
JP Rosevear <jp@nubinary.com>
2024-02-23 20:50:03 -0500

    feat: Support pre-bundling for core with esbuild plugin

--- packages/vite-plugin-rhino/src/index.ts
+++ packages/vite-plugin-rhino/src/index.ts
@@ -14,15 +14,48 @@ const LIGHT_LOGO_MODULE_ID = 'assets/images/logo-light.svg';
 const ENV_MODULE_ID = 'virtual:@rhino-project/config/env';
 const RESOLVED_ENV_MODULE_ID = '\0' + ENV_MODULE_ID;
 
+const jsPattern =
+  /components\/app\/CustomPrimaryNavigation|components\/app\/CustomSecondaryNavigation|models\/static|routes\/custom/;
+
+const svgPattern =
+  /assets\/images\/logo-dark.svg|assets\/images\/logo-light.svg/;
+
+// ESBuild is used to pre-bundle modules in dev mode
+// This plugin is used to resolve the virtual/local import paths for Rhino
+// in @rhino-project/core - at some point we should support @rhino-project/config as well
+const esbuildRhinoPlugin = {
+  name: 'esbuild-rhino-plugin',
+  setup(build) {
+    build.onResolve({ filter: jsPattern }, async (args) => {
+      return {
+        path: path.resolve(__dirname, 'src', `${args.path}.js`)
+      };
+    });
+
+    // ESBuild doesn't support SVG imports, so we use the Vite technique of marking them as external
+    //https://github.com/vitejs/vite/blob/42fd11c1c6d37402bd15ba816fbf65dbed3abe55/packages/vite/src/node/optimizer/esbuildDepPlugin.ts#L166
+    build.onResolve({ filter: svgPattern }, async (args) => {
+      return {
+        path: path.resolve(__dirname, 'src', args.path),
+        external: true
+      };
+    });
+  }
+};
+
 export function RhinoProjectVite(): Plugin {
   let CONFIG: ResolvedConfig;
 
   return {
     name: 'vite-plugin-rhino',
+    enforce: 'pre',
     config: () => ({
       optimizeDeps: {
+        esbuildOptions: {
+          plugins: [esbuildRhinoPlugin]
+        },
         // Don't process with esbuild so that we can use resolveId for rhino.config
-        exclude: ['@rhino-project/config', '@rhino-project/core']
+        exclude: ['@rhino-project/config']
       }
     }),
 
@@ -31,6 +64,7 @@ export function RhinoProjectVite(): Plugin {
     },
 
     resolveId(id) {
+      console.log('resolveId', id);
       if (id === CONFIG_MODULE_ID) {
         // Replace 'rhino.config' with the path to the local file
         // FIXME: Allow the location to be configured
