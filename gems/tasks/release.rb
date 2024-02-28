8d03903e9a3b67630e0e022f84f84bdade474172
JP Rosevear <jp@nubinary.com>
2024-02-28 13:46:01 -0500

    fix: Debug

--- gems/tasks/release.rb
+++ gems/tasks/release.rb
@@ -3,7 +3,6 @@
 # Order dependent. E.g. rhino_notifications depends on rhino_jobs.
 FRAMEWORKS = %w(
 rhino_project_core
-rhino_project_organizations
 rhino_project_jobs
 rhino_project_notifications
 rhino_project_subscriptions
