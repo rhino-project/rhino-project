1593525e7c965bf1741daf8ddba32ee807608f6f
JP Rosevear <jp@nubinary.com>
2024-02-28 12:21:01 -0500

    chore: Prettier scripts

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,34 +1,34 @@
 // @ts-check
 
-import jsonfile from 'jsonfile'
+import jsonfile from 'jsonfile';
 
 /** @param {string} version */
-export const releaseCommitMsg = (version) => `release: v${version}`
+export const releaseCommitMsg = (version) => `release: v${version}`;
 
 /** @param {string} str */
 export const capitalize = (str) => {
-  return str.slice(0, 1).toUpperCase() + str.slice(1)
-}
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+};
 
 /**
  * @param {string} pathName
  * @returns {Promise<import('type-fest').PackageJson>}
  */
 export const readPackageJson = async (pathName) => {
-  return await jsonfile.readFile(pathName)
-}
+  return await jsonfile.readFile(pathName);
+};
 
 /**
  * @param {string} pathName
  * @param {(json: import('type-fest').PackageJson) => Promise<void> | void} transform
  */
 export const updatePackageJson = async (pathName, transform) => {
-  const json = await readPackageJson(pathName)
-  await transform(json)
+  const json = await readPackageJson(pathName);
+  await transform(json);
   await jsonfile.writeFile(pathName, json, {
-    spaces: 2,
-  })
-}
+    spaces: 2
+  });
+};
 
 /**
  * @template TItem
@@ -37,14 +37,14 @@ export const updatePackageJson = async (pathName, transform) => {
  */
 export const getSorterFn = (sorter) => {
   return (a, b) => {
-    const sortedA = sorter(a)
-    const sortedB = sorter(b)
+    const sortedA = sorter(a);
+    const sortedB = sorter(b);
     if (sortedA > sortedB) {
-      return 1
+      return 1;
     }
     if (sortedA < sortedB) {
-      return -1
+      return -1;
     }
-    return 0
-  }
-}
+    return 0;
+  };
+};
