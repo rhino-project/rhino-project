48e4a9d8fad4e8bac3140788f18e558865f528eb
JP Rosevear <jp@nubinary.com>
2024-02-28 13:55:27 -0500

    fix: diff

--- gems/tasks/release.rb
+++ gems/tasks/release.rb
@@ -55,6 +55,7 @@ directory "pkg"
     end
 
     task gem => %w(update_versions pkg) do
+      sh "echo git diff"
       cmd = ""
       cmd += "cd #{framework} && " unless framework == "rhino_project"
       cmd += "bundle exec rake package && " unless framework == "rhino_project"
