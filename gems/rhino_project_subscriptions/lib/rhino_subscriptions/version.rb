a8b0bc943bf82bc5c48e500bfc4b9ffd4b0fe4b9
JP Rosevear <jprosevear@users.noreply.github.com>
2024-02-28 20:53:31 +0000

    release: v0.20.0-beta.29

--- gems/rhino_project_subscriptions/lib/rhino_subscriptions/version.rb
+++ gems/rhino_project_subscriptions/lib/rhino_subscriptions/version.rb
@@ -10,7 +10,7 @@ module RhinoSubscriptions
     MAJOR = 0
     MINOR = 20
     TINY  = 0
-    PRE   = "beta.24"
+    PRE   = "beta.29"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
