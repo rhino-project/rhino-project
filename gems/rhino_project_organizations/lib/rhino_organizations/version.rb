000ed9592991a072b829d53be254a6cd9111ee2a
JP Rosevear <jprosevear@users.noreply.github.com>
2024-03-01 13:50:07 +0000

    release: v0.20.0-beta.34

--- gems/rhino_project_organizations/lib/rhino_organizations/version.rb
+++ gems/rhino_project_organizations/lib/rhino_organizations/version.rb
@@ -10,7 +10,7 @@ module RhinoOrganizations
     MAJOR = 0
     MINOR = 20
     TINY  = 0
-    PRE   = "beta.29"
+    PRE   = "beta.34"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
