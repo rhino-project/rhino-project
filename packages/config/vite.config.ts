f57c9d739d82366a65ccc6c534f18a9fb5321026
JP Rosevear <jp@nubinary.com>
2024-03-04 12:37:23 -0500

    chore: Upgrade tanstack and config

--- packages/config/vite.config.ts
+++ packages/config/vite.config.ts
@@ -1,28 +1,23 @@
 import { tanstackBuildConfig } from '@tanstack/config/build';
 import { defineConfig, mergeConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
 const config = defineConfig({
-  plugins: [
-    externalizeDeps({
-      include: [
-        'rhino.config',
-        'virtual:@rhino-project/config/env',
-        'virtual:@rhino-project/config/assets'
-      ]
-    }),
-    react()
-  ],
+  plugins: [react()],
   test: {
     watch: false
   }
 });
 
 export default mergeConfig(
+  config,
   tanstackBuildConfig({
     entry: ['./src/index.ts', './src/env.ts', './src/assets.ts'],
-    srcDir: './src'
-  }),
-  config
+    srcDir: './src',
+    externalDeps: [
+      'rhino.config',
+      'virtual:@rhino-project/config/env',
+      'virtual:@rhino-project/config/assets'
+    ]
+  })
 );
