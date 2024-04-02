2b3c759b80a78d277f2c39cf0e29cfd5b34def82
JP Rosevear <jp@nubinary.com>
2024-04-02 06:52:07 -0400

    feat: Single devtool instance at top level listening to broadcasts

--- packages/core/src/components/models/ModelEditProvider.js
+++ packages/core/src/components/models/ModelEditProvider.js
@@ -1,18 +1,19 @@
 import { createContext } from 'react';
-import env from '@rhino-project/config/env';
 import { FormProvider } from '../forms/FormProvider';
 import { ModelContext } from './ModelProvider';
-import { RhinoDevTool } from '../devtool/RhinoDevTool';
+import { useRhinoDevBroadcastSend } from '../../hooks/dev';
 
 export const ModelEditContext = createContext();
 
 export const ModelEditProvider = ({ children, ...props }) => {
-  const { methods, model } = props;
+  const { methods, ...context } = props;
+  const { model } = context;
+
+  useRhinoDevBroadcastSend({ type: 'ModelEditContext', context });
 
   return (
     <ModelContext.Provider value={{ model }}>
       <ModelEditContext.Provider value={{ ...props }}>
-        {env.MODE === 'development' && <RhinoDevTool />}
         <FormProvider {...methods}>{children}</FormProvider>
       </ModelEditContext.Provider>
     </ModelContext.Provider>
--- packages/core/src/components/models/ModelShowProvider.js
+++ packages/core/src/components/models/ModelShowProvider.js
@@ -1,18 +1,19 @@
 import { createContext } from 'react';
-import env from '@rhino-project/config/env';
 import { FormProvider } from '../forms/FormProvider';
 import { ModelContext } from './ModelProvider';
-import { RhinoDevTool } from '../devtool/RhinoDevTool';
+import { useRhinoDevBroadcastSend } from '../../hooks/dev';
 
 export const ModelShowContext = createContext();
 
 export const ModelShowProvider = ({ children, ...props }) => {
-  const { methods, model } = props;
+  const { methods, ...context } = props;
+  const { model } = context;
+
+  useRhinoDevBroadcastSend({ type: 'ModelShowContext', context });
 
   return (
     <ModelContext.Provider value={{ model }}>
       <ModelShowContext.Provider value={{ ...props }}>
-        {env.MODE === 'development' && <RhinoDevTool />}
         <FormProvider {...methods}>{children}</FormProvider>
       </ModelShowContext.Provider>
     </ModelContext.Provider>
