f57b94e0ca1a1622f281022ee35b872fec8aa490
JP Rosevear <jprosevear@users.noreply.github.com>
2024-06-10 17:48:55 +0000

    release: v0.20.0-beta.73

--- gems/rhino_project_notifications/lib/rhino_notifications/version.rb
+++ gems/rhino_project_notifications/lib/rhino_notifications/version.rb
@@ -10,7 +10,7 @@ module RhinoNotifications
     MAJOR = 0
     MINOR = 20
     TINY  = 0
-    PRE   = "beta.72"
+    PRE   = "beta.73"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
