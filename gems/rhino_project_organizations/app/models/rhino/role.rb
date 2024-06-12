006459e7a10c5b74d83b2b1f443f1d3bdea41973
JP Rosevear <jp@codalio.com>
2024-06-12 06:47:59 -0400

    fix: allow underscore and make nice display name for role

--- gems/rhino_project_organizations/app/models/rhino/role.rb
+++ gems/rhino_project_organizations/app/models/rhino/role.rb
@@ -4,8 +4,12 @@ module Rhino
   class Role < ApplicationRecord
     self.abstract_class = true
 
-    validates :name, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z]+\z/, message: "alpha characters only" }
+    validates :name, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z_]+\z/, message: "alpha characters only" }
 
     rhino_owner_global
+
+    def display_name
+      name.titleize
+    end
   end
 end
