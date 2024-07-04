4941a48ab1e9669772081dda6a299941eac74004
JP Rosevear <jp@codalio.com>
2024-07-04 05:53:21 -0400

    fix: build absolute path like model index table

--- packages/core/src/__tests__/components/models/cells/ModelCellIdentifier.spec.js
+++ packages/core/src/__tests__/components/models/cells/ModelCellIdentifier.spec.js
@@ -1,6 +1,45 @@
+import { render } from '@testing-library/react';
+import {
+  ModelIndexSimple,
+  ModelIndexTable
+} from '../../../../components/models';
 import { ModelCellIdentifier } from '../../../../components/models/cells/ModellCellIdentifier';
+import { createWrapper } from '../../../shared/helpers';
 import { sharedCellTests } from './sharedCellTests';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 describe('ModelCellIdentifier', () => {
+  const Wrapper = ({ children, ...props }) => {
+    const queryClient = new QueryClient();
+
+    return (
+      <MemoryRouter {...props}>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/:baseOwnerId/*" element={<>{children}</>} />
+          </Routes>
+        </QueryClientProvider>
+      </MemoryRouter>
+    );
+  };
+
   sharedCellTests(ModelCellIdentifier);
+
+  it('renders absolute link', () => {
+    const { asFragment } = render(
+      <ModelIndexSimple
+        model="blog"
+        queryOptions={{ initialData: { results: [{ id: 1 }] } }}
+      >
+        <ModelIndexTable paths={['id']} />
+      </ModelIndexSimple>,
+      {
+        wrapper: createWrapper(Wrapper, {
+          initialEntries: ['/1']
+        })
+      }
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
