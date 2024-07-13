c13ecf1b53f2d486c737613e39ec1a9979da1d8f
JP Rosevear <jprosevear@users.noreply.github.com>
2024-07-13 21:40:18 +0000

    release: v0.20.0-beta.102

--- gems/rhino_project_jobs/lib/rhino_jobs/version.rb
+++ gems/rhino_project_jobs/lib/rhino_jobs/version.rb
@@ -10,7 +10,7 @@ module RhinoJobs
     MAJOR = 0
     MINOR = 20
     TINY  = 0
-    PRE   = "beta.101"
+    PRE   = "beta.102"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
--- gems/rubocop-rhino-project/lib/rubocop/rhino_project/version.rb
+++ gems/rubocop-rhino-project/lib/rubocop/rhino_project/version.rb
@@ -10,7 +10,7 @@ module RubocopRhinoProject
     MAJOR = 0
     MINOR = 20
     TINY  = 0
-    PRE   = "beta.101"
+    PRE   = "beta.102"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
