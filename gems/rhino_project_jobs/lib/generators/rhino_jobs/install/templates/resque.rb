fc56e5be36d7783f120c904b20e1d3c4165d07a9
JP Rosevear <jp@codalio.com>
2024-09-30 17:51:35 -0400

    fix: support TLS redis on Heroku

--- gems/rhino_project_jobs/lib/generators/rhino_jobs/install/templates/resque.rb
+++ gems/rhino_project_jobs/lib/generators/rhino_jobs/install/templates/resque.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  Resque.redis = ENV['REDIS_URL'] || 'localhost:6379'
+  Resque.redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379', ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } )
 end
