92404ac3e24baf180f75a538b1c9ae129658e3ed
JP Rosevear <jp@codalio.com>
2024-10-03 11:55:37 -0400

    feat: initial dark theme support

--- packages/core/src/pages/auth/AuthPage.js
+++ packages/core/src/pages/auth/AuthPage.js
@@ -1,19 +1,19 @@
 import { Target } from '../../components/layouts';
-import { DarkLogo } from '../../components/logos';
 
 // External Dependencies
 import PropTypes from 'prop-types';
 import { Col, Container, Row } from 'reactstrap';
 import { useGlobalComponentForModel } from '../../hooks/overrides';
+import { ThemedLogo } from '../../components/logos';
 
 const AuthPageBase = ({ children, description }) => {
   return (
     <Container className="h-100">
       <Target>
-        <Row className="p-3 bg-white shadow rounded">
+        <Row className="p-3 shadow rounded">
           <Col className="my-auto" md="6">
             <div className="auth-logo">
-              <DarkLogo />
+              <ThemedLogo />
             </div>
             {description}
           </Col>
