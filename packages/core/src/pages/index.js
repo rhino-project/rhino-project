fbbeab10e18792f08185a9e47efaa3851d34d29b
JP Rosevear <jp@codalio.com>
2024-10-03 07:35:31 -0400

    feat: export model pages for external use

--- packages/core/src/pages/index.js
+++ packages/core/src/pages/index.js
@@ -1,2 +1,8 @@
 export * from './BaseAuthedPage';
 export * from './BasePage';
+
+export { Create as ModelCreatePage } from './model/Create';
+export { Edit as ModelEditPage } from './model/Edit';
+export { Index as ModelIndexPage } from './model/Index';
+export { Show as ModelShowPage } from './model/Show';
+export { ModelPage } from './model/ModelPage';
