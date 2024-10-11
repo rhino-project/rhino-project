73a4aafadb001f9678ed4523bb3fc1e01a7ce23b
JP Rosevear <jprosevear@users.noreply.github.com>
2024-10-11 18:09:02 +0000

    release: v0.21.0-beta.37

--- gems/rhino_project_jobs/lib/rhino_jobs/version.rb
+++ gems/rhino_project_jobs/lib/rhino_jobs/version.rb
@@ -10,7 +10,7 @@ module RhinoJobs
     MAJOR = 0
     MINOR = 21
     TINY  = 0
-    PRE   = "beta.35"
+    PRE   = "beta.37"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
--- gems/rhino_project_notifications/lib/rhino_notifications/version.rb
+++ gems/rhino_project_notifications/lib/rhino_notifications/version.rb
@@ -10,7 +10,7 @@ module RhinoNotifications
     MAJOR = 0
     MINOR = 21
     TINY  = 0
-    PRE   = "beta.35"
+    PRE   = "beta.37"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
