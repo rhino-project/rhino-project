1ab8a54687f452d1fdbce9eabde025e92847abf4
JP Rosevear <jp@codalio.com>
2024-11-17 13:38:30 -0500

    fix: subscription migration lint

--- gems/rhino_project_subscriptions/db/migrate/20210302299092_rhino_alter_stripe_customers.rb
+++ gems/rhino_project_subscriptions/db/migrate/20210302299092_rhino_alter_stripe_customers.rb
@@ -1,7 +1,6 @@
 class RhinoAlterStripeCustomers < ActiveRecord::Migration[6.0]
   def change
-
-      remove_reference :stripe_customers, :user,  index: true
-      add_reference :stripe_customers, :base_owner
-    end
+    remove_reference :stripe_customers, :user,  index: true
+    add_reference :stripe_customers, :base_owner
+  end
 end
