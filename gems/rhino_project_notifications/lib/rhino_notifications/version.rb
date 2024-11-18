999bdb7c1ff8394a6c0c5696c1ecc8387e09edf7
JP Rosevear <jprosevear@users.noreply.github.com>
2024-11-18 18:12:06 +0000

    release: v0.22.0-beta.1

--- gems/rhino_project_notifications/lib/rhino_notifications/version.rb
+++ gems/rhino_project_notifications/lib/rhino_notifications/version.rb
@@ -10,7 +10,7 @@ module RhinoNotifications
     MAJOR = 0
     MINOR = 22
     TINY  = 0
-    PRE   = "alpha.6"
+    PRE   = "beta.1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
--- gems/rhino_project_organizations/lib/rhino_organizations/version.rb
+++ gems/rhino_project_organizations/lib/rhino_organizations/version.rb
@@ -10,7 +10,7 @@ module RhinoOrganizations
     MAJOR = 0
     MINOR = 22
     TINY  = 0
-    PRE   = "alpha.6"
+    PRE   = "beta.1"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
