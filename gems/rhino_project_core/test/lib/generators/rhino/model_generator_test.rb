3580df83e687b9a2bab945c7e1e743ab3e3bdce2
JP Rosevear <jp@codalio.com>
2024-11-20 07:37:44 -0500

    chore: generate model more aligned with current rubocop

--- gems/rhino_project_core/test/lib/generators/rhino/model_generator_test.rb
+++ gems/rhino_project_core/test/lib/generators/rhino/model_generator_test.rb
@@ -26,13 +26,13 @@ class ModelGeneratorTest < Rails::Generators::TestCase
   def test_model_with_owner_option
     run_generator ["device", "name:string", "device_group:references", "--owner=device_group"]
     assert_file File.join(destination_root, "app/models/device.rb"), /rhino_owner :device_group/
-    assert_file File.join(destination_root, "app/models/device.rb"), /rhino_references %i\[device_group\]/
+    assert_file File.join(destination_root, "app/models/device.rb"), /rhino_references %i\[ device_group \]/
   end
 
   def test_model_with_base_owner_as_owner_option
     run_generator ["device_group", "name:string", "user:references", "--owner=user"]
     assert_file File.join(destination_root, "app/models/device_group.rb"), /rhino_owner_base/
-    assert_file File.join(destination_root, "app/models/device_group.rb"), /rhino_references %i\[user\]/
+    assert_file File.join(destination_root, "app/models/device_group.rb"), /rhino_references %i\[ user \]/
   end
 
   def test_model_with_global_owner_as_owner_option
@@ -48,7 +48,7 @@ class ModelGeneratorTest < Rails::Generators::TestCase
   def test_model_with_multiple_references
     run_generator ["my_multiple_reference_model", "name:string", "category:references", "blog:references", "--owner=blog"]
     assert_file File.join(destination_root, "app/models/my_multiple_reference_model.rb"), /rhino_owner :blog/
-    assert_file File.join(destination_root, "app/models/my_multiple_reference_model.rb"), /rhino_references %i\[category blog\]/
+    assert_file File.join(destination_root, "app/models/my_multiple_reference_model.rb"), /rhino_references %i\[ category blog \]/
   end
 
   private
