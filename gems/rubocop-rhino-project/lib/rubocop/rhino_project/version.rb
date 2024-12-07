d9623711b75615b9bc19e893763f5ae271ed2af7
JP Rosevear <jprosevear@users.noreply.github.com>
2024-12-07 16:46:40 +0000

    release: v0.22.0-beta.7

--- gems/rubocop-rhino-project/lib/rubocop/rhino_project/version.rb
+++ gems/rubocop-rhino-project/lib/rubocop/rhino_project/version.rb
@@ -10,7 +10,7 @@ module RubocopRhinoProject
     MAJOR = 0
     MINOR = 22
     TINY  = 0
-    PRE   = "beta.6"
+    PRE   = "beta.7"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
   end
