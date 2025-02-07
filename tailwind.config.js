eace38aa8e80a301d36d3d4893243ad16a58840b
JP Rosevear <jp@codalio.com>
2025-02-07 09:34:42 -0500

    chore: rename ui-nextui to ui-heroui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./packages/ui-nextui/src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./packages/ui-heroui/src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {}
   },
