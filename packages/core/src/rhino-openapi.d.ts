13bc716ce173a4db6f3c9b273bcc283b12e4dca6
JP Rosevear <jp@codalio.com>
2025-02-09 15:53:17 -0500

    chore: fix eslint after typescript linter upgrade

--- packages/core/src/rhino-openapi.d.ts
+++ packages/core/src/rhino-openapi.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export namespace OpenAPI {
   // OpenAPI extensions can be declared using generics
