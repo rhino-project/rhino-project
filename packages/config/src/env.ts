bdf01256078c511727619c711d84afd20fcb53b4
JP Rosevear <jp@codalio.com>
2025-03-28 10:18:08 -0400

    chore: do not use vite's DEV/PROD for production detection

--- packages/config/src/env.ts
+++ packages/config/src/env.ts
@@ -4,8 +4,11 @@ const RhinoRuntimeEnv: RhinoEnv = {
   ...env,
 
   // Compound env vars
-  DESIGN_SYSTEM_ENABLED: env.DEV || env.DESIGN_SYSTEM_ENABLED === 'true',
-  ROLLBAR_ENV: env.ROLLBAR_ENV || env.MODE,
-  ROLLBAR_ENABLED: env.PROD || env.ROLLBAR_ENABLED === 'true'
+  DESIGN_SYSTEM_ENABLED:
+    process.env.NODE_ENV === 'development' ||
+    env.DESIGN_SYSTEM_ENABLED === 'true',
+  ROLLBAR_ENV: env.ROLLBAR_ENV || process.env.NODE_ENV || 'development',
+  ROLLBAR_ENABLED:
+    process.env.NODE_ENV === 'production' || env.ROLLBAR_ENABLED === 'true'
 };
 export default RhinoRuntimeEnv;
