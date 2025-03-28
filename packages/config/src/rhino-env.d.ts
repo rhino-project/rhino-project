262f2ea8e93418df1fd4a2f043bf3e799bccf49f
JP Rosevear <jp@codalio.com>
2025-03-28 15:30:19 -0400

    fix: make rhino-env.d.ts as standalone interface

--- packages/config/src/rhino-env.d.ts
+++ packages/config/src/rhino-env.d.ts
@@ -1,20 +1,7 @@
-export interface RhinoEnv {
+interface RhinoEnv {
   readonly DESIGN_SYSTEM_ENABLED: boolean;
   readonly ROLLBAR_ACCESS_TOKEN?: string;
   readonly ROLLBAR_ENV: string;
   readonly ROLLBAR_ENABLED: boolean;
   readonly STRIPE_PUBLISHABLE_KEY?: string;
-}
-
-// Convert RhinoEnv to a type that can be used in the browser
-export type RhinoWindowEnv = {
-  readonly [K in keyof RhinoEnv]?: string;
-};
-
-declare global {
-  interface Window {
-    rhino: {
-      env: RhinoWindowEnv;
-    };
-  }
 }
