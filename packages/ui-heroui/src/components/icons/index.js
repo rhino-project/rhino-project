1e92a5c4982528cc0e14be06d9f3b96b2be160ae
JP Rosevear <jp@codalio.com>
2025-04-19 10:04:50 -0400

    fix: set default NavIcon size to 16

--- packages/ui-heroui/src/components/icons/index.js
+++ packages/ui-heroui/src/components/icons/index.js
@@ -1,3 +1,3 @@
 import { Icon } from '@iconify/react';
 
-export const NavIcon = (props) => <Icon height={24} width={24} {...props} />;
+export const NavIcon = (props) => <Icon height={16} width={16} {...props} />;
