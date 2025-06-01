fa8374feb45b0bd5a8bc56412b3b34c0823a82b2
JP Rosevear <699616+jprosevear@users.noreply.github.com>
2025-06-01 08:48:22 -0400

    feat: simplify

--- gems/rhino_project_core/lib/rhino/routing/mapper.rb
+++ gems/rhino_project_core/lib/rhino/routing/mapper.rb
@@ -3,25 +3,21 @@
 module Rhino
   module Routing
     module Mapper
-      extend ActiveSupport::Concern
+      def rhino_resources(resource_class, **options)
+        defaults = {
+          path: resource_class.route_path,
+          controller: resource_class.controller_name,
+          only: resource_class.routes,
+          rhino_resource: resource_class.name,
+          format: false
+        }
 
-      class_methods do
-        def rhino_resources(resource_class, **options)
-          defaults = {
-            path: resource_class.route_path,
-            controller: resource_class.controller_name,
-            only: resource_class.routes,
-            rhino_resource: resource_class.name,
-            format: false
-          }
+        route_options = defaults.merge(options)
 
-          route_options = defaults.merge(options)
-
-          if resource_class.route_singular?
-            resource resource_class.route_key, **route_options
-          else
-            resources resource_class.route_key, **route_options
-          end
+        if resource_class.route_singular?
+          resource resource_class.route_key, **route_options
+        else
+          resources resource_class.route_key, **route_options
         end
       end
     end
