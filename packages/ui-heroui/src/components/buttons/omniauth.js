6ac94f9156af22f5639d28e2df6102b6ee70314c
Bruno Cipolla <brunocmoreira@live.com>
2025-12-03 02:26:40 -0300

    fix/iconify-icons-wrong-name

--- packages/ui-heroui/src/components/buttons/omniauth.js
+++ packages/ui-heroui/src/components/buttons/omniauth.js
@@ -1,28 +1,26 @@
 import { useMemo } from 'react';
-import { OmniIconButton } from './';
-
 import PropTypes from 'prop-types';
 
 const providerOverrides = {
   auth0: {
     name: 'Auth0',
-    icon: 'door-open'
+    icon: 'mdi:door-open'
   },
   azure_oauth2: {
     name: 'Microsoft',
-    icon: 'microsoft'
+    icon: 'mdi:microsoft'
   },
   developer: {
     name: 'Developer',
-    icon: 'code-square'
+    icon: 'mdi:code-square'
   },
   google_oauth2: {
     name: 'Google',
-    icon: 'google'
+    icon: 'mdi:google'
   },
   github_oauth2: {
     name: 'GitHub',
-    icon: 'github'
+    icon: 'mdi:github'
   }
 };
 
